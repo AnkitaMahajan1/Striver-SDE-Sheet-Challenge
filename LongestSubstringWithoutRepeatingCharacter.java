54adda8cedc7e80f072518d96a0bfc1a3e4fbaae
Ankita Mahajan <61576746+AnkitaMahajan1@users.noreply.github.com>
2022-06-14 07:57:52 +0530

    Create LongestSubstringWithoutRepeatingCharacter.java

---
+++ LongestSubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,22 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+	public static int uniqueSubstrings(String s) 
+    {
+		//write your code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0;
+        int len = 0;
+        int n = s.length();
+        while(r < n){
+            if(map.containsKey(s.charAt(r))){
+               l = Math.max(l, map.get(s.charAt(r)) + 1); 
+            }
+            map.put(s.charAt(r), r);
+            len = Math.max(len, r - l + 1);
+            r++;
+        }
+        return len;
+	}
+}
