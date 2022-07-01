ef8960603ce7e7004a6da4c5918926b4e691bf2b
Ankita Mahajan <61576746+AnkitaMahajan1@users.noreply.github.com>
2022-07-01 22:22:36 +0530

    Create CombinationSum.java

---
+++ CombinationSum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        solve(0, candidates, target, ans, ds);
+        return ans;
+    }
+    
+    void solve(int start, int[] candidates, int target, List<List<Integer>> ans, List<Integer> ds){
+        // base case
+        // target reached or more than target
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        // no hope further to find target.
+        if(target<0){
+            return;
+        }
+        
+        for(int i = start; i<candidates.length; i++){
+            ds.add(candidates[i]);
+            solve(i, candidates, target-candidates[i], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        return;
+    }
+}
